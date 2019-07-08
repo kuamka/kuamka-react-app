68705c14fa1f11c804668b00b0c0fd679cee60f2
Steve Baxter <steve@kuamka.com>
2019-07-08 16:11:23 +0100

    Enabling test coverage and storyshots

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,9 @@
-import { configure } from '@storybook/react';
+import { configure } from "@storybook/react";
+import requireContext from "require-context.macro";
 
 // Automatically import all files ending in *.stories.tsx. Uses webpack's require.context which can
 // be statically resolved by the bundler. This has to be polyfilled under node when storyshotting.
-const req = require.context("../src", true, /.stories.tsx$/);
+const req = requireContext("../src", true, /.stories.tsx$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
